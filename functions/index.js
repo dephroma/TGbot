306942b07177c342ecbc83c87981edc9540196ed
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-02-26 01:53:21 +0300

    4141

--- functions/index.js
+++ functions/index.js
@@ -24,12 +24,12 @@ const bot = new Telegraf(BOT_TOKEN);
 const app = express();
 app.use(express.json());
 
-exports.handler = async (event) => {
+exports.handler = async function (event, context) {
     return {
-      statusCode: 200,
-      body: "Hello, World!",
+        statusCode: 200,
+        body: "Hello Netlify!",
     };
-  };
+};
 
 // Настройка webhook для получения обновлений
 app.post(`/${BOT_TOKEN}`, (req, res) => {
