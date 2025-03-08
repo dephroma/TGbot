0c0f8a2ad099be06a6034399dd9d60037e629088
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-08 05:53:31 +0300

    webhookHandler

---
+++ functions/webhookHandler.js
@@ -0,0 +1,44 @@
+// const { Telegraf } = require('telegraf');
+
+//! Обработчик вебхука
+const handleWebhook = async (event) => {
+    const body = JSON.parse(event.body); //* Получаем тело запроса
+    try {
+        console.log('Received webhook event:', body); //* Логируем событие
+        await bot.handleUpdate(body); //* Обрабатываем обновление через Telegraf
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Webhook processed successfully" })
+        };
+    } catch (error) {
+        console.error('Error handling webhook:', error); //* Логируем ошибку
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error handling webhook' })
+        };
+    }
+};
+
+// const handleWebhook = async (event, context) => {
+//     const body = JSON.parse(event.body);  //* Получаем тело запроса
+//     try {
+//         console.log('Received webhook event:', body);  //* Логируем событие
+//         await bot.handleUpdate(body);  //* Обрабатываем обновление через Telegraf
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify({ message: "Webhook processed successfully" })
+//         };
+//     } catch (error) {
+//         console.error('Error handling webhook:', error);  //* Логируем ошибку
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({ message: 'Error handling webhook' })
+//         };
+//     }
+// };
+// // Обработчик для AWS Lambda или другого сервиса
+// exports.handler = async (event, context) => {
+//     return handleWebhook(event); // Используем обработчик вебхука
+// };
+
+module.exports = { handleWebhook };
