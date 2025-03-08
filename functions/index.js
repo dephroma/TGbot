107b9e91c9c6e4095abd24111f7c95198ec47b1c
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-08 06:29:14 +0300

    ОТКАТ

--- functions/index.js
+++ functions/index.js
@@ -69,20 +69,20 @@ bot.on('text', async (ctx) => {
 // exports.handler = async (event, context) => {
 //     return handleWebhook(event); };
 //! Обработчик для webhook
-// exports.handler = async (event, context) => {
-//     const body = JSON.parse(event.body);  //* Получаем тело запроса
-//     try {
-//         console.log('Received webhook event:', body);  //* Логируем событие
-//         await bot.handleUpdate(body);  //* Обрабатываем обновление через Telegraf
-//         return {
-//             statusCode: 200,
-//             body: JSON.stringify({ message: "Webhook processed successfully" })
-//         };
-//     } catch (error) {
-//         console.error('Error handling webhook:', error);  //* Логируем ошибку
-//         return {
-//             statusCode: 500,
-//             body: JSON.stringify({ message: 'Error handling webhook' })
-//         };
-//     }
-// };
+exports.handler = async (event, context) => {
+    const body = JSON.parse(event.body);  //* Получаем тело запроса
+    try {
+        console.log('Received webhook event:', body);  //* Логируем событие
+        await bot.handleUpdate(body);  //* Обрабатываем обновление через Telegraf
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Webhook processed successfully" })
+        };
+    } catch (error) {
+        console.error('Error handling webhook:', error);  //* Логируем ошибку
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error handling webhook' })
+        };
+    }
+};
