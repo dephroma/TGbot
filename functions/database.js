0051bf83b51f08b2b774f9706692b05ae9a903a6
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-10 02:08:57 +0300

    MongoClient

--- functions/database.js
+++ functions/database.js
@@ -1,16 +1,26 @@
-const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB подключена');
-    } catch (err) {
-        console.error('Ошибка подключения к MongoDB:', err);
-        process.exit(1);
-    }
-};
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://rromann192:<Prevedmened192!>@cluster0.qdr3m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-module.exports = connectDB;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
