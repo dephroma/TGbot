09ae9d8de65045b2100c8df5a93339baff387235
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-10 02:43:59 +0300

    async function connectDB()

--- functions/database.js
+++ functions/database.js
@@ -10,7 +10,7 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
+async function connectDB() {
   try {
     //* Connect the client to the server	(optional starting in v4.7)
     await client.connect();
