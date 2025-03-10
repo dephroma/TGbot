0008d177a16eed12e22980ee5fe0bfcbfb32f79c
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-10 05:56:24 +0300

    async function connectDB

--- functions/database.js
+++ functions/database.js
@@ -18,24 +18,20 @@ const client = new MongoClient(uri, {
 });
 
 async function connectDB() {
-  try {
-    console.log("Строка подключения:", uri);
-if (!uri.startsWith("mongodb+srv://")) {
-  throw new Error("Ошибка: строка подключения MongoDB неверного формата!");
-}
-    //* Connect the client to the server	(optional starting in v4.7)
     try {
-        await client.connect();
-        console.log("✅ Успешно подключились к MongoDB!");
+      console.log("Строка подключения:", uri);
+      if (!uri.startsWith("mongodb+srv://")) {
+        throw new Error("Ошибка: строка подключения MongoDB неверного формата!");
+      }
+  
+      await client.connect();
+      console.log("✅ Успешно подключились к MongoDB!");
+  
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } catch (error) {
-        console.error("❌ Ошибка подключения:", error);
+      console.error("❌ Ошибка подключения:", error);
     }
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    //* Ensures that the client will close when you finish/error
-    // await client.close();
   }
-}
 
 module.exports = connectDB;
