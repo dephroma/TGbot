b5be81d09ca5acd38c7c2387868cb96eb41c4fa4
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-10 05:54:11 +0300

    ЛОГИ

--- functions/database.js
+++ functions/database.js
@@ -24,13 +24,17 @@ if (!uri.startsWith("mongodb+srv://")) {
   throw new Error("Ошибка: строка подключения MongoDB неверного формата!");
 }
     //* Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    //* Send a ping to confirm a successful connection
+    try {
+        await client.connect();
+        console.log("✅ Успешно подключились к MongoDB!");
+    } catch (error) {
+        console.error("❌ Ошибка подключения:", error);
+    }
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     //* Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 
