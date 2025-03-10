29fb6bcc5a1ff42dd4e89301803b07df8ab0dbaa
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-10 20:17:47 +0300

    датаБАЗА

--- functions/database.js
+++ functions/database.js
@@ -1,12 +1,43 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGO_URI;
+
+// if (!uri) {
+//     console.error("Ошибка: строка подключения MONGO_URI не найдена!");
+//   } else {
+//     console.log("Строка подключения:", uri);
+//   }
+
+// //* Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function connectDB() {
+//     try {
+//       console.log("Строка подключения:", uri);
+//       if (!uri.startsWith("mongodb+srv://")) {
+//         throw new Error("Ошибка: строка подключения MongoDB неверного формата!");
+//       }
+  
+//       await client.connect();
+//       console.log("✅ Успешно подключились к MongoDB!");
+  
+//       await client.db("admin").command({ ping: 1 });
+//       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     } catch (error) {
+//       console.error("❌ Ошибка подключения:", error);
+//     }
+//   }
+
+// module.exports = connectDB;
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
-
-if (!uri) {
-    console.error("Ошибка: строка подключения MONGO_URI не найдена!");
-  } else {
-    console.log("Строка подключения:", uri);
-  }
 
 //* Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,20 +49,16 @@ const client = new MongoClient(uri, {
 });
 
 async function connectDB() {
-    try {
-      console.log("Строка подключения:", uri);
-      if (!uri.startsWith("mongodb+srv://")) {
-        throw new Error("Ошибка: строка подключения MongoDB неверного формата!");
-      }
-  
-      await client.connect();
-      console.log("✅ Успешно подключились к MongoDB!");
-  
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } catch (error) {
-      console.error("❌ Ошибка подключения:", error);
-    }
+  try {
+    //* Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //* Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    //* Ensures that the client will close when you finish/error
+    await client.close();
   }
+}
 
 module.exports = connectDB;
