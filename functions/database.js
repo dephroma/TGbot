6f1b0ce02eb89da2f6bb8c5582830d338ac1a1d4
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-12 01:36:23 +0300

    catch (error) database БЕЗ ЗАКРЫТИЯ СОЕДИНЕНИЯ

--- functions/database.js
+++ functions/database.js
@@ -35,11 +35,42 @@
 //   }
 
 // module.exports = connectDB;
+// require('dotenv').config();
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGO_URI;
+
+// //* Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function connectDB() {
+//   try {
+//     //* Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     //* Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } catch (error) {
+//     console.error('Ошибка подключения к базе данных:', error);
+//   } finally {
+//     //* Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+
+// module.exports = connectDB;
+
+
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 
-//* Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Создаем клиента MongoClient с опциями для API
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -50,15 +81,15 @@ const client = new MongoClient(uri, {
 
 async function connectDB() {
   try {
-    //* Connect the client to the server	(optional starting in v4.7)
+    // Подключаем клиента к серверу MongoDB
     await client.connect();
-    //* Send a ping to confirm a successful connection
+    // Отправляем команду ping для проверки соединения
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    //* Ensures that the client will close when you finish/error
-    await client.close();
+  } catch (error) {
+    console.error('Ошибка подключения к базе данных:', error);
   }
 }
 
+// Не закрываем соединение с базой данных, чтобы оно оставалось открытым
 module.exports = connectDB;
