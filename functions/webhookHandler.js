d14ad1d724fcef72fea52ffb84e0c8eade7ec305
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-03-13 21:11:39 +0300

    41414

--- functions/webhookHandler.js
+++ functions/webhookHandler.js
@@ -6,21 +6,34 @@ const bot = new Telegraf(BOT_TOKEN);
 bot.telegram.setWebhook('https://tgeagle.netlify.app/.netlify/functions/index');
 
 const handleWebhook = async (event, context) => {
-    const body = JSON.parse(event.body); //* Получаем тело запроса
+    console.log("Получено событие:", event); // Логируем событие для отладки
+
+    let body;
     try {
-        console.log('Received webhook event:', body); //* Логируем событие
-        await bot.handleUpdate(body); //* Обрабатываем обновление через Telegraf
+        if (event.body) {
+            body = JSON.parse(event.body); // Если есть тело запроса, парсим его
+            console.log("Parsed body:", body); // Логируем тело запроса после парсинга
+        } else {
+            console.log("Тело запроса пустое!"); // Логируем, если тело запроса пустое
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Empty request body" })
+            };
+        }
+        
+        await bot.handleUpdate(body); // Обрабатываем обновление через Telegraf
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "Webhook processed successfully" })
         };
     } catch (error) {
-        console.error('Error handling webhook:', error); //* Логируем ошибку
+        console.error('Ошибка при обработке webhook:', error); // Логируем ошибку
         return {
             statusCode: 500,
             body: JSON.stringify({ message: 'Error handling webhook' })
         };
     }
 };
+
 
 module.exports = { bot, handleWebhook };
